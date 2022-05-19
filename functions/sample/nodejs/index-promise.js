edf7b35ac48e51ab7944545fa769b2587625261a
AsifMahsud <asifmahsud543@gmail.com>
2022-05-19 12:52:55 +0530

    Update index-promise.js

--- functions/sample/nodejs/index-promise.js
+++ functions/sample/nodejs/index-promise.js
@@ -18,13 +18,49 @@ function main(params) {
 }
 
 function getDbs(cloudant) {
-    return new Promise((resolve, reject) => {
-        cloudant.getAllDbs();
-            .then(body => {
-                resolve({ dbs: body.result });
-            })
-            .catch(err => {
+     return new Promise((resolve, reject) => {
+         cloudant.getAllDbs()
+             .then(body => {
+                 resolve({ dbs: body.result });
+             })
+             .catch(err => {
+                  console.log(err);
+                 reject({ err: err });
+             });
+     });
+ }
+ 
+ 
+ /*
+ Sample implementation to get the records in a db based on a selector. If selector is empty, it returns all records. 
+ eg: selector = {state:"Texas"} - Will return all records which has value 'Texas' in the column 'State'
+ */
+ function getMatchingRecords(cloudant,dbname, selector) {
+     return new Promise((resolve, reject) => {
+         cloudant.postFind({db:dbname,selector:selector})
+                 .then((result)=>{
+                   resolve({result:result.result.docs});
+                 })
+                 .catch(err => {
+                    console.log(err);
+                     reject({ err: err });
+                 });
+          })
+ }
+ 
+                        
+ /*
+ Sample implementation to get all the records in a db.
+ */
+ function getAllRecords(cloudant,dbname) {
+     return new Promise((resolve, reject) => {
+         cloudant.postAllDocs({ db: dbname, includeDocs: true, limit: 10 })            
+             .then((result)=>{
+               resolve({result:result.result.rows});
+             })
+             .catch(err => {
+                console.log(err);
                 reject({ err: err });
-            });
-    });
-}
+             });
+         })
+ }
