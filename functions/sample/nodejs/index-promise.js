1d69d122227056ce39236f3f1fda020260a19424
AsifMahsud <asifmahsud543@gmail.com>
2022-05-19 12:12:37 +0530

    Update index-promise.js
    Changed to the new package Clouding version

--- functions/sample/nodejs/index-promise.js
+++ functions/sample/nodejs/index-promise.js
@@ -2,14 +2,16 @@
  * Get all dealerships
  */
 
-const Cloudant = require('@cloudant/cloudant');
+const { CloudantV1 } = require('@ibm-cloud/cloudant');
+const { IamAuthenticator } = require('ibm-cloud-sdk-core');
 
 function main(params) {
 
-    const cloudant = Cloudant({
-        url: params.COUCH_URL,
-        plugins: { iamauth: { iamApiKey: params.IAM_API_KEY } }
+    const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY })
+    const cloudant = CloudantV1.newInstance({
+      authenticator: authenticator
     });
+    cloudant.setServiceUrl(params.COUCH_URL);
 
     let dbListPromise = getDbs(cloudant);
     return dbListPromise;
@@ -17,9 +19,9 @@ function main(params) {
 
 function getDbs(cloudant) {
     return new Promise((resolve, reject) => {
-        cloudant.db.list()
+        cloudant.getAllDbs();
             .then(body => {
-                resolve({ dbs: body });
+                resolve({ dbs: body.result });
             })
             .catch(err => {
                 reject({ err: err });
