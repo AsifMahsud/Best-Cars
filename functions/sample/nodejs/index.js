6f3f50c6cfe30f97eb5ad7d2762463f4af3b8a70
AsifMahsud <asifmahsud543@gmail.com>
2022-11-14 19:07:29 -0800

    adding node.js linting

--- functions/sample/nodejs/index.js
+++ functions/sample/nodejs/index.js
@@ -2,14 +2,13 @@
  * Get all databases
  */
 
-const { CloudantV1 } = require('@ibm-cloud/cloudant');
-const { IamAuthenticator } = require('ibm-cloud-sdk-core');
+const { CloudantV1 } = require("@ibm-cloud/cloudant");
+const { IamAuthenticator } = require("ibm-cloud-sdk-core");
 
 function main(params) {
-
-  const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY })
+  const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY });
   const cloudant = CloudantV1.newInstance({
-      authenticator: authenticator
+    authenticator: authenticator,
   });
   cloudant.setServiceUrl(params.COUCH_URL);
 
@@ -18,10 +17,14 @@ function main(params) {
 }
 
 function getDbs(cloudant) {
-    cloudant.getAllDbs().then((body) => {
-        body.forEach((db) => {
-            dbList.push(db);
-        });
-    }).catch((err) => { console.log(err); });
+  cloudant
+    .getAllDbs()
+    .then((body) => {
+      body.forEach((db) => {
+        dbList.push(db);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
-
